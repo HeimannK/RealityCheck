1fa099c624b0c957d72d29c873d282904d6e765f
HeimannK <37170193+HeimannK@users.noreply.github.com>
2024-04-22 17:04:24 +1000

    Basic application creation
    Added the Reality Check logo, added dependencies, and components for the basic application to run.

---
+++ src/components/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import logo from '../logo.png';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      buffer: null 
+    };
+  }
+
+  captureFile = (event) => {
+    event.preventDefault()
+    // process file for IPFS
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.setState({buffer: Buffer(reader.result)})
+    }
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    console.log("Submitting the form...")
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-light bg-dark p-0 shadow text-white">
+        <h1>Reality Check</h1>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <img src={logo} className="App-logo" alt="logo" width="150px" />
+                <p>&nbsp;</p>
+                <h1>Reality Check</h1>
+                <p>&nbsp;</p>
+                <div className="wrapper">
+                <div className="form">
+                  <form onSubmit={this.onSubmit}> 
+                    <p>
+                      <label htmlFor="url">Enter the URL:</label>
+                      <input type="url" />
+                    </p>
+                    <p>
+                      <label htmlFor="file">Upload an Image:</label>
+                      <input type="file" onChange={this.captureFile}/>
+                    </p>
+                    <p>
+                      <input type="button" value="Cancel" />
+                      <input type="submit" value="Submit" />
+                    </p>
+                  </form>
+                </div>
+                </div>                
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
