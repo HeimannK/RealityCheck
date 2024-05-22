c0c46381655db99c21be7b4b8572192824f74795
idg-salman <163240400+idg-salman@users.noreply.github.com>
2024-05-23 00:53:37 +0600

    Delete src directory

--- src/components/ipfs.js
+++
@@ -1,5 +0,0 @@
-const IPFS= require('ipfs-api')
-// const { prototype } = require('react-bootstrap/lib/Accordion')
-const ipfs= new IPFS({ host: 'https://api.pinata.cloud/pinning/pinFileToIPFS', port: 5001, protocol: 'https'})
-
-export default ipfs;
